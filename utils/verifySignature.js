2ed63ae7ddb9a830e2e11d7b002f131c5ca8af19
Muhammed Nihal <nihal514t@gmail.com>
2026-08-02 15:59:27 +0530

    Safer signature

--- utils/verifySignature.js
+++ utils/verifySignature.js
@@ -1,30 +1,26 @@
 const crypto = require("crypto");
 
-/**
- * Verify Razorpay Webhook Signature
- *
- * @param {Buffer} rawBody
- * @param {string} signature
- * @returns {boolean}
- */
 function verifySignature(rawBody, signature) {
-    try {
-        const generatedSignature = crypto
-            .createHmac(
-                "sha256",
-                process.env.RAZORPAY_WEBHOOK_SECRET
-            )
-            .update(rawBody)
-            .digest("hex");
 
-        return crypto.timingSafeEqual(
-            Buffer.from(generatedSignature, "utf8"),
-            Buffer.from(signature, "utf8")
-        );
-    } catch (error) {
-        console.error("Signature Verification Error:", error.message);
+    const generatedSignature = crypto
+        .createHmac(
+            "sha256",
+            process.env.RAZORPAY_WEBHOOK_SECRET
+        )
+        .update(rawBody)
+        .digest("hex");
+
+    // Prevent timingSafeEqual from throwing
+    if (
+        generatedSignature.length !== signature.length
+    ) {
         return false;
     }
+
+    return crypto.timingSafeEqual(
+        Buffer.from(generatedSignature, "utf8"),
+        Buffer.from(signature, "utf8")
+    );
 }
 
 module.exports = verifySignature;
